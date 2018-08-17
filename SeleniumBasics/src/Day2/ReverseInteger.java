708850846c01cf1a04507bc68283e059890e45b8
shra24 <s,rane24@gmail.com>
2018-08-17 17:02:33 +0530

    added class

---
+++ SeleniumBasics/src/Day2/ReverseInteger.java
@@ -0,0 +1,16 @@
+package Day2;
+
+public class ReverseInteger {
+
+	public static void main(String[] args) {
+	
+		
+		int i = 12345 ;
+		
+		StringBuffer sf = new StringBuffer (String.valueOf(i)) ;
+		
+		System.out.println(sf.reverse());
+
+	}
+
+}
